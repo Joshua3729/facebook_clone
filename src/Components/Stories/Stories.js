f8adb924afea0d889aea89be4dc7e032388532f4
Joshua <3729344@myuwc.ac.za>
2022-07-09 13:06:29 +0200

    added to Story component

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -1,8 +1,17 @@
 import React from "react";
 import classes from "./Stories.module.css";
+import Story from "./Story/Story";
 
 const Stories = () => {
-  return <div className={classes.Stories}></div>;
+  return (
+    <div className={classes.Stories}>
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+    </div>
+  );
 };
 
 export default Stories;
--- src/Components/Stories/Story/Story.js
+++ src/Components/Stories/Story/Story.js
@@ -2,7 +2,16 @@ import React from "react";
 import classes from "./Story.module.css";
 
 const Story = () => {
-  return <div className={classes.Story}></div>;
+  return (
+    <div className={classes.Story}>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYH_VDaGfxQ_cPhkgDPyoxXJgnnKHzEw7kdg&usqp=CAU"
+        alt=""
+        className={classes.profile}
+      />
+      <p className={classes.name}>Sive Wana</p>
+    </div>
+  );
 };
 
 export default Story;
