1935582e8816332affa859b2d3ee5d5554793a23
Joshua <3729344@myuwc.ac.za>
2022-07-09 09:49:14 +0200

    added Logo

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Navigation.module.css";
 
 const Navigation = () => {
-  return;
+  return <div className={classes.Navigation}></div>;
 };
 
 export default Navigation;
