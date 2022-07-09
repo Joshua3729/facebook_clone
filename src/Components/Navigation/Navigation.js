15d51c33095b1789fd694dbcda50c15d190f51de
Joshua <3729344@myuwc.ac.za>
2022-07-09 10:18:54 +0200

    added to Navigation component

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Logo from "../UI/Logo/Logo";
 import classes from "./Navigation.module.css";
+import search from "../../Assets/Images/search.png";
 
 const Navigation = () => {
   return (
     <div className={classes.Navigation}>
-      <Logo />
+      <div className={classes.first_column}>
+        <Logo />
+        <button className={classes.search_btn}>
+          <img src={search} alt="" />
+        </button>
+      </div>
     </div>
   );
 };
