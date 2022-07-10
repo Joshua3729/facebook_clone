d859a8eb3d9430609b317113fdfc39379fb6f780
Joshua <3729344@myuwc.ac.za>
2022-07-10 03:43:16 +0200

    added to post component

--- src/Components/Posts/Post/Post.js
+++ src/Components/Posts/Post/Post.js
@@ -2,7 +2,24 @@ import React from "react";
 import classes from "./Post.module.css";
 
 const Post = () => {
-  return;
+  return (
+    <div className={classes.Post}>
+      <div className={classes.post_header}>
+        <div className={classes.profile_wrapper}>
+          <img
+            src="https://variety.com/wp-content/uploads/2021/09/Drake-publicity9-2021-e1631312720295.jpg?w=681&h=383&crop=1"
+            alt="profile"
+            className={classes.profile}
+          />
+        </div>
+
+        <div className={classes.name_wrapper}>
+          <div className={classes.user_name}>Drake</div>
+          <div className={classes.time}>16h</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Posts;
+export default Post;
--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,8 +1,13 @@
 import React from "react";
 import classes from "./Posts.module.css";
+import Post from "./Post/Post";
 
 const Posts = () => {
-  return;
+  return (
+    <div className={classes.Posts}>
+      <Post />
+    </div>
+  );
 };
 
 export default Posts;
