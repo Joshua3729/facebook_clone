6f4f74f9a4b7c094669c4871904d6c181e7ac5d3
Joshua <3729344@myuwc.ac.za>
2022-07-14 14:39:58 +0200

    added to Modal component

--- src/Pages/AuthPage/AuthPage.js
+++ src/Pages/AuthPage/AuthPage.js
@@ -1,52 +1,63 @@
 import React from "react";
+import Modal from "../../Components/Modal/Modal";
 import classes from "./AuthPage.module.css";
 
 const AuthPage = () => {
-  return (
-    <div className={classes.AuthPage}>
-      <div className={classes.innerWrapper}>
-        <div className={classes.left_wrapper}>
-          <div className={classes.text_wrapper}>
-            <h1 className={classes.logo}>bookface</h1>
-            <h3 className={classes.slogan}>
-              Bookface helps you connect and share with the people in your life.
-            </h3>
-          </div>
-        </div>
-        <div className={classes.right_wrapper}>
-          <div className={classes.signIn_input}>
-            <div
-              className={[classes.email_wrapper, classes.input_item].join(" ")}
-            >
-              <input
-                type="email"
-                className={classes.email_address}
-                placeholder="Email address"
-              />
-            </div>
-            <div
-              className={[classes.password_wrapper, classes.input_item].join(
-                " "
-              )}
-            >
-              <input
-                type="password"
-                className={classes.password}
-                placeholder="Password"
-              />
-            </div>
-            <button className={classes.LogIn_btn}>Log In</button>
+  const show = true;
 
-            <div className={classes.fogort_password}>Forgotten password?</div>
-            <div className={classes.createAccount_wrapper}>
-              <button className={classes.createAccount_btn}>
-                Create New Account
-              </button>
+  return (
+    <>
+      <Modal show={show}>
+        <div className={classes.modal_wrapper}></div>
+      </Modal>
+      <div className={classes.AuthPage}>
+        <div className={classes.innerWrapper}>
+          <div className={classes.left_wrapper}>
+            <div className={classes.text_wrapper}>
+              <h1 className={classes.logo}>bookface</h1>
+              <h3 className={classes.slogan}>
+                Bookface helps you connect and share with the people in your
+                life.
+              </h3>
+            </div>
+          </div>
+          <div className={classes.right_wrapper}>
+            <div className={classes.signIn_input}>
+              <div
+                className={[classes.email_wrapper, classes.input_item].join(
+                  " "
+                )}
+              >
+                <input
+                  type="email"
+                  className={classes.email_address}
+                  placeholder="Email address"
+                />
+              </div>
+              <div
+                className={[classes.password_wrapper, classes.input_item].join(
+                  " "
+                )}
+              >
+                <input
+                  type="password"
+                  className={classes.password}
+                  placeholder="Password"
+                />
+              </div>
+              <button className={classes.LogIn_btn}>Log In</button>
+
+              <div className={classes.fogort_password}>Forgotten password?</div>
+              <div className={classes.createAccount_wrapper}>
+                <button className={classes.createAccount_btn}>
+                  Create New Account
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
