a55d1d77e666948cbe9e92dff54d2e5d500afe50
Joshua <3729344@myuwc.ac.za>
2022-07-14 09:29:44 +0200

    added to Modal component

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,2 +1,26 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+import Background from "../Background/Background";
+
+const Modal = (props) => {
+  return ReactDOM.createPortal(
+    <>
+      <Background show={props.show} clicked={props.clicked} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show
+            ? "translate(-50%, -50%) scale(1)"
+            : "translate(-50%,-50%) scale(0)",
+          opacity: props.show ? "1" : "1",
+        }}
+      >
+        {props.children}
+      </div>
+    </>,
+    document.getElementById("modal")
+  );
+};
+
+export default Modal;
