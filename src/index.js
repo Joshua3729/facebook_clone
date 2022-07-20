e9b975787fa9515eb626b1d16779907d9d860028
Joshua <3729344@myuwc.ac.za>
2022-07-20 15:43:32 +0200

    added react-redux

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import thunk from "redux-thunk";
+import Home from "./store/Home";
 
 const store = createStore(
-  burgerBuilderReducer,
+  Home,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
