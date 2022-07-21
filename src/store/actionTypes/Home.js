fc133c833c459cf3bb09c729b4002a1f2e0c84fe
Joshua <3729344@myuwc.ac.za>
2022-07-21 12:15:10 +0200

    added to home acetions

---
+++ src/store/actionTypes/Home.js
@@ -0,0 +1,6 @@
+export const Home = (ingredients) => {
+  return {
+    type: actionTypes.SET_POSTS,
+    posts: posts,
+  };
+};
--- src/store/reducer/Home.js
+++ src/store/reducer/Home.js
@@ -1,6 +1,15 @@
+import * as actionTypes from "../actions/actionTypes";
+
 const initialState = {
   posts: [],
 };
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GETSTATE:
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
