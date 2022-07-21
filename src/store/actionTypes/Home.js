f32d8de1b8c0cc7bfb852964609b76a0b1dbad0e
Joshua <3729344@myuwc.ac.za>
2022-07-21 13:05:49 +0200

    added index file

--- src/store/actionTypes/Home.js
+++ src/store/actionTypes/Home.js
@@ -1,6 +1,23 @@
-export const Home = (ingredients) => {
+export const setPosts = (posts) => {
   return {
     type: actionTypes.SET_POSTS,
     posts: posts,
+  };
+};
+
+export const initPosts = () => {
+  return (dispatch) => {
+    fetch("http://localhost:5000/feed/get_posts")
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("Failed to fetch posts.");
+        }
+
+        return res.json();
+      })
+      .then((resData) => {
+        dispatch(setPosts(resData.posts));
+      })
+      .catch((err) => console.log(err));
   };
 };
