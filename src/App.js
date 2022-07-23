c850c78d135c5b84c4918daf2ca49451ab07d882
Joshua <3729344@myuwc.ac.za>
2022-07-23 22:49:11 +0200

    added setSessionData action to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./App.module.css";
 import Navigation from "./Components/Navigation/Navigation";
 import Home from "./Pages/Home/Home";
@@ -13,10 +13,37 @@ import User_profile from "./Pages/User_Profile/User_profile";
 import AuthPage from "./Pages/AuthPage/AuthPage";
 import Footer from "./Components/Footer/Footer";
 import { useDispatch, useSelector } from "react-redux";
+import * as HomeActions from "./store/actionTypes/index";
 
 function App() {
   const isAuth = useSelector((state) => state.auth.isAuth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const session_data = JSON.parse(localStorage.getItem("session_data"));
 
+    if (!session_data?.token) {
+      return;
+    }
+    if (new Date(session_data?.xpiryDate) <= new Date()) {
+      this.logoutHandler();
+      return;
+    }
+
+    const remainingMilliseconds =
+      new Date(session_data?.expiryDate).getTime() - new Date().getTime();
+    this.setAutoLogout(remainingMilliseconds);
+
+    if (session_data?.user_data.user_id) {
+      isAuth = true;
+    }
+    dispatch(
+      HomeActions.setSessionData(
+        isAuth,
+        session_data.token,
+        session_data.user_data
+      )
+    );
+  }, []);
   let route = (
     <Router>
       <Routes>
--- src/store/actionTypes/index.js
+++ src/store/actionTypes/index.js
@@ -1,2 +1,7 @@
-export { onLogin, setOnInputBlur, setOnInputChange } from "./Auth";
+export {
+  onLogin,
+  setSessionData,
+  setOnInputBlur,
+  setOnInputChange,
+} from "./Auth";
 export { initPosts, getNewPost } from "./Home";
