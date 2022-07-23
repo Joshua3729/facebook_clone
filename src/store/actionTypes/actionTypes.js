405990a3b28d0eb2f8d1f02bf7b7349d455426d8
Joshua <3729344@myuwc.ac.za>
2022-07-23 16:52:43 +0200

    added to Auth reducer

--- src/store/actionTypes/actionTypes.js
+++ src/store/actionTypes/actionTypes.js
@@ -1,2 +1,5 @@
 export const SET_POSTS = "SET_POSTS";
 export const GET_NEW_POST = "GET_NEW_POST";
+export const ON_INPUT_CHANGE = "ON_INPUT_CHANGE";
+export const ON_INPUT_BLUR = "ON_INPUT_BLUR";
+export const ON_LOGIN = "ON_LOGIN";
--- src/store/reducer/Auth.js
+++ src/store/reducer/Auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actionTypes/actionTypes";
+import { required, length, email } from "../../Utils/validators";
 
 const initialState = {
   Auth: false,
@@ -64,6 +65,56 @@ const initialState = {
 };
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.ON_INPUT_CHANGE:
+      let isValid = true;
+      let validators =
+        action.formType === "loginForm"
+          ? state.loginForm[action.input].validators
+          : state.signupForm[action.input].validators;
+      for (const validator of validators) {
+        isValid = isValid && validator(action.value);
+      }
+      let previousState =
+        action.formType === "loginForm" ? state.loginForm : state.signupForm;
+      const updatedForm = {
+        ...previousState,
+        [action.input]: {
+          ...previousState[action.input],
+          valid: isValid,
+          value: action.value,
+        },
+      };
+      let formIsValid = true;
+      for (const inputName in updatedForm) {
+        if (inputName !== "formIsValid") {
+          formIsValid = formIsValid && updatedForm[inputName].valid;
+        }
+      }
+      return {
+        ...state,
+        [action.formType]: updatedForm,
+        formIsValid: formIsValid,
+      };
+
+    case actionTypes.ON_INPUT_BLUR:
+      return {
+        ...state,
+        loginForm: {
+          ...state.loginForm,
+          [action.input]: {
+            ...state.loginForm[action.input],
+            touched: true,
+          },
+        },
+      };
+    case actionTypes.ON_LOGIN:
+      return {
+        ...state,
+        isAuth: true,
+        token: resData.token,
+        authLoading: false,
+        userId: resData.userId,
+      };
     default:
       return state;
   }
