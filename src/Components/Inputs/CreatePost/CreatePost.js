458b1d688c75fb6f02e2a424635d61054fc3970a
Joshua <3729344@myuwc.ac.za>
2022-07-25 12:57:02 +0200

    added to the CreatePost component

---
+++ src/Components/Inputs/CreatePost/CreatePost.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./CreatePost";
+
+const CreatePost = (props) => {
+  return <div className={classes.CreatePost}></div>;
+};
+
+export default CreatePost;
