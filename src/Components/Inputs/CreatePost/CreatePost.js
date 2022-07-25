16cc3abfc151cc46d7ae5a72483c561a603c2163
Joshua <3729344@myuwc.ac.za>
2022-07-25 13:59:48 +0200

    added to CreatePost

--- src/Components/Inputs/CreatePost/CreatePost.js
+++ src/Components/Inputs/CreatePost/CreatePost.js
@@ -4,7 +4,24 @@ import classes from "./CreatePost.module.css";
 const CreatePost = (props) => {
   return (
     <div className={classes.CreatePost}>
-      <div className={classes.CreatePost_header}>Create Post</div>
+      <div className={classes.CreatePost_header}>
+        Create Post
+        <div className={classes.close}>
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/yX/r/TdCEremeWv5.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={classes.user_wrapper}>
+        <div className={classes.profile_wrapper}>
+          <img src={props.profile_img} alt="" />
+        </div>
+        <div className={classes.right_wrapper}>
+          <div className={classes.user_name}>{props.fullname}</div>
+          <div className={classes.visibility_marker}></div>
+        </div>
+      </div>
     </div>
   );
 };
