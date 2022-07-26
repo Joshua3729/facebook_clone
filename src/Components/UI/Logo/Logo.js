0b7deaf734f8ec147b5f513311c19e2d6cfd9b92
Joshua <3729344@myuwc.ac.za>
2022-07-26 16:59:38 +0200

    added LoadingPage

--- src/Components/UI/Logo/Logo.js
+++ src/Components/UI/Logo/Logo.js
@@ -1,8 +1,10 @@
 import React from "react";
 import classes from "./Logo.module.css";
+import logo from "../../../Assets/Images/logo.svg";
 
 const Logo = () => {
-  return <div className={classes.Logo}>B</div>;
+  // return <div className={classes.Logo}>B</div>;
+  return <img src={logo} alt="logo" className={classes.logo_svg} />;
 };
 
 export default Logo;
