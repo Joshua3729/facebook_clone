0a2c5d9422334ffd2b0f0375d5c5b8b2ef96328f
Joshua <3729344@myuwc.ac.za>
2022-07-30 00:05:46 +0200

    added Add Profile Image component

--- src/Pages/User_Profile/User_profile.js
+++ src/Pages/User_Profile/User_profile.js
@@ -9,6 +9,7 @@ import { useSelector } from "react-redux";
 import Posts from "../../Components/Posts/Posts";
 
 import add_friend from "../../Assets/Images/add_friend.png";
+import Modal from "../../Components/Modal/Modal";
 
 const User_profile = () => {
   // const user_id = props.match?.params.user_id;
@@ -19,6 +20,7 @@ const User_profile = () => {
   const username = `${user_id[0]} ${user_id[1]}`;
   const [user_posts, setUser_posts] = useState(null);
   const [user_data, setUserData] = useState(null);
+  const [show_imageUpload_modal, set_show_imageUpload_modal] = useState(true);
   const [friends, setFriends_status] = useState(true);
   useEffect(() => {
     getUserData();
@@ -88,69 +90,75 @@ const User_profile = () => {
   );
 
   return (
-    <div className={classes.User_profile}>
-      <div className={classes.header_bar}>
-        <div className={classes.cover_wrapper}></div>
-        <div className={classes.profile_wrapper}>
-          <div className={classes.header_innerWrapper}>
-            <div className={classes.userImg}>
-              <img src={user_data?.profile_img} alt="" />
+    <>
+      <Modal show={show_imageUpload_modal}></Modal>
+      <div className={classes.User_profile}>
+        <div className={classes.header_bar}>
+          <div className={classes.cover_wrapper}></div>
+          <div className={classes.profile_wrapper}>
+            <div className={classes.header_innerWrapper}>
+              <div className={classes.userImg}>
+                <img src={user_data?.profile_img} alt="" />
+                <div className={classes.addImage_wrapper}>
+                  <i className={classes.addImage_icon}></i>
+                </div>
+              </div>
+              <div className={classes.userName}>{username}</div>
             </div>
-            <div className={classes.userName}>{username}</div>
-          </div>
-          <div className={classes.btns_wrapper}>
-            <button
-              className={[
-                classes.add_friend,
-                friends ? classes.inactive_btn : classes.active_btn,
-              ].join(" ")}
-              disabled={friends}
-            >
-              {friend_button}
-            </button>
-            <button
-              className={[
-                classes.messanger,
-                friends ? classes.active_btn : classes.inactive_btn,
-              ].join(" ")}
-              disabled={!friends}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                className={classes.message}
+            <div className={classes.btns_wrapper}>
+              <button
+                className={[
+                  classes.add_friend,
+                  friends ? classes.inactive_btn : classes.active_btn,
+                ].join(" ")}
+                disabled={friends}
               >
-                <path d="M256.55 8C116.52 8 8 110.34 8 248.57c0 72.3 29.71 134.78 78.07 177.94 8.35 7.51 6.63 11.86 8.05 58.23A19.92 19.92 0 0 0 122 502.31c52.91-23.3 53.59-25.14 62.56-22.7C337.85 521.8 504 423.7 504 248.57 504 110.34 396.59 8 256.55 8zm149.24 185.13l-73 115.57a37.37 37.37 0 0 1-53.91 9.93l-58.08-43.47a15 15 0 0 0-18 0l-78.37 59.44c-10.46 7.93-24.16-4.6-17.11-15.67l73-115.57a37.36 37.36 0 0 1 53.91-9.93l58.06 43.46a15 15 0 0 0 18 0l78.41-59.38c10.44-7.98 24.14 4.54 17.09 15.62z" />
-              </svg>{" "}
-              Message
-            </button>
+                {friend_button}
+              </button>
+              <button
+                className={[
+                  classes.messanger,
+                  friends ? classes.active_btn : classes.inactive_btn,
+                ].join(" ")}
+                disabled={!friends}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className={classes.message}
+                >
+                  <path d="M256.55 8C116.52 8 8 110.34 8 248.57c0 72.3 29.71 134.78 78.07 177.94 8.35 7.51 6.63 11.86 8.05 58.23A19.92 19.92 0 0 0 122 502.31c52.91-23.3 53.59-25.14 62.56-22.7C337.85 521.8 504 423.7 504 248.57 504 110.34 396.59 8 256.55 8zm149.24 185.13l-73 115.57a37.37 37.37 0 0 1-53.91 9.93l-58.08-43.47a15 15 0 0 0-18 0l-78.37 59.44c-10.46 7.93-24.16-4.6-17.11-15.67l73-115.57a37.36 37.36 0 0 1 53.91-9.93l58.06 43.46a15 15 0 0 0 18 0l78.41-59.38c10.44-7.98 24.14 4.54 17.09 15.62z" />
+                </svg>{" "}
+                Message
+              </button>
+            </div>
+          </div>
+          <div className={classes.nav_bar}>
+            <ul className={classes.links_wrapper}>
+              <li className={[classes.nav_item, classes.active].join(" ")}>
+                Posts
+              </li>
+              <li className={classes.nav_item}>About</li>
+              <li className={classes.nav_item}>Friends</li>
+              <li className={classes.nav_item}>Photos</li>
+              <li className={classes.nav_item}>Videos</li>
+            </ul>
           </div>
         </div>
-        <div className={classes.nav_bar}>
-          <ul className={classes.links_wrapper}>
-            <li className={[classes.nav_item, classes.active].join(" ")}>
-              Posts
-            </li>
-            <li className={classes.nav_item}>About</li>
-            <li className={classes.nav_item}>Friends</li>
-            <li className={classes.nav_item}>Photos</li>
-            <li className={classes.nav_item}>Videos</li>
-          </ul>
+        <div className={classes.main_wrapper}>
+          <div className={classes.main}>
+            <div className={classes.left_wrapper}>
+              <IntroCard />
+              <PhotosCard />
+            </div>
+            <div className={classes.right_wrapper}>
+              <WhatsOnYourMind />
+              <Posts posts={user_posts} />
+            </div>
+          </div>
         </div>
       </div>
-      <div className={classes.main_wrapper}>
-        <div className={classes.main}>
-          <div className={classes.left_wrapper}>
-            <IntroCard />
-            <PhotosCard />
-          </div>
-          <div className={classes.right_wrapper}>
-            <WhatsOnYourMind />
-            <Posts posts={user_posts} />
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
