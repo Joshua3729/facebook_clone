662b4bfa02b68e939b1d9b7f1957d1f52636b40b
Joshua <3729344@myuwc.ac.za>
2022-08-16 02:30:48 +0200

    added notification item component

--- src/Components/NotificationPop_up/NotificationPop_up.js
+++ src/Components/NotificationPop_up/NotificationPop_up.js
@@ -3,10 +3,11 @@ import Notification_item from "../Notification_item/Notification_item";
 import classes from "./NotificationPop_up.module.css";
 
 const NotificationPop_up = (props) => {
-  return (
+  return ReactDOM.createPortal(
     <div className={classes.NotificationPop_up_wrapper}>
       <Notification_item userNotification={props.userNotification} />
-    </div>
+    </div>,
+    document.getElementById("notification")
   );
 };
 
