95ec5f9efad0cbea4af8fde1dfa2b27d852246a0
Joshua <3729344@myuwc.ac.za>
2022-08-16 02:06:49 +0200

    added notification item component

---
+++ src/Components/Notification_item/Notification_item.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Notification_item.module.css";
+
+const Notification_item = (props) => {
+  return <div className={classes.notification_item}></div>;
+};
+
+export default Notification_item;
