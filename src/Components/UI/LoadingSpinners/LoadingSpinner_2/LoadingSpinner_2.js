355febfb5cf511abc865ff39982955d0aa11e2e4
Joshua <3729344@myuwc.ac.za>
2022-08-18 13:29:24 +0200

    added LoadingSpinner_2 component

---
+++ src/Components/UI/LoadingSpinners/LoadingSpinner_2/LoadingSpinner_2.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./LoadingSpinner_2.module.css";
+
+const LoadingSpinner_2 = () => {
+  return (
+    <svg className={classes.loader} viewBox="0 0 24 24">
+      <circle class="loader__value" cx="12" cy="12" r="10" />
+      <circle class="loader__value" cx="12" cy="12" r="10" />
+      <circle class="loader__value" cx="12" cy="12" r="10" />
+      <circle class="loader__value" cx="12" cy="12" r="10" />
+      <circle class="loader__value" cx="12" cy="12" r="10" />
+      <circle class="loader__value" cx="12" cy="12" r="10" />
+    </svg>
+  );
+};
+
+export default LoadingSpinner_2;
