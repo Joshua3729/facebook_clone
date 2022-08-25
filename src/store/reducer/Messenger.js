0dc3ce0ba1dc62a9756200e8f3a0ea2526215731
Joshua <3729344@myuwc.ac.za>
2022-08-25 14:41:34 +0200

    added Messenger reducer

---
+++ src/store/reducer/Messenger.js
@@ -0,0 +1,12 @@
+import * as actionTypes from "../actionTypes/actionTypes";
+import { required, length, email } from "../../Utils/validators";
+
+const initialState = {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
